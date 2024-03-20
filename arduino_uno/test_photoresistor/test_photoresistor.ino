05b17266042262789d6c91b97d1721091364b924
razor7877 <razor7877@gmail.com>
2024-03-20 13:06:28 -0400

    Added average light measure on serial input

--- arduino_uno/test_photoresistor/test_photoresistor.ino
+++ arduino_uno/test_photoresistor/test_photoresistor.ino
@@ -1,15 +1,46 @@
+//#define DEBUG_SERIAL // Add this define to print out debug info to the serial port
+
 int analogIn = A0; // The photoresistor input pin
-int light_intensity = 0;
+
+int index = 0;
+const int array_size = 100;
+int light_values[array_size] = {};
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  light_intensity = analogRead(analogIn);
+  //light_intensity = analogRead(analogIn);
   
   if (Serial.available() > 0) {
+#ifdef DEBUG_SERIAL
+    Serial.println("Received measure key");
+#endif
+
     Serial.read(); // Empty serial buffer
-    Serial.println(light_intensity);
+
+    while (index++ < array_size - 1) {
+#ifdef DEBUG_SERIAL
+      Serial.println(index);
+#endif
+
+      light_values[index] = analogRead(analogIn);
+      delay(1);
+    }
+
+    float avg_light = 0.0f;
+    for (int i = 0; i < array_size; i++) {
+      avg_light += light_values[i];
+
+#ifdef DEBUG_SERIAL
+      Serial.println("Calculating average");
+#endif
+    }
+
+    avg_light /= array_size;
+    index = 0;
+
+    Serial.println(avg_light);
   }
 }
